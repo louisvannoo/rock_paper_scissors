2ea1fddb2dd0265cf01681f1500324627f2d5fdb
Louis Vannoorenberghe <louis.vannoo@gmail.com>
2022-11-13 17:58:50 +0100

    add playRound

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random() * choices.length)]
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection) {
+        return "It's a draw !";
+    } else if(((playerSelection.toLowerCase() == 'rock') && (computerSelection == 'paper')) || ((playerSelection.toLowerCase() == 'paper') && (computerSelection == 'scissors')) || ((playerSelection.toLowerCase() == 'scissors') && (computerSelection == 'rock'))) {
+        return `You lost! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
+
